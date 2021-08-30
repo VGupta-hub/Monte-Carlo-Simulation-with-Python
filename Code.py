f3c45d867a72227640797f097709962fbbbf49f3
Vaishali Gupta <83883988+VGupta-hub@users.noreply.github.com>
2021-08-30 14:19:07 -0400

    Update Code.py
    Monte Carlo Simulation is based on repeated random sampling. 
    
    Monte Carlo has 3 main usages: estimate parameters or statistical measures, examine the properties of the estimates, approximate integrals
    
    Using Monte Carlo Simulation, I have written a quick code to predict future stock prices outcomes for AAPL using Python. I have taken references for medium.com in this project.
    
    Please feel free to play around the code.

--- Code.py
+++ Code.py
@@ -1,3 +1,4 @@
+# Importing the libraries
 import numpy as np
 import pandas as pd
 from pandas_datareader import data as web
@@ -7,24 +8,29 @@ import scipy
 from scipy.stats import norm
 %matplotlib notebook
 
+# Pulling the data for AAPL (I have used a CSV file and data from 01/01/2021 - 08/27/2021)
 df = pd.read_csv("C:/Users/vgupt/Desktop/Website Project Folder/MCS/AAPL.csv")['Adj Close']
 df
 
+#Ploting the prices on a graph
 plt.xlabel("Date")
 plt.ylabel("Price")
 df.plot(figsize=(15,6))
 
+# Calulating the logarithmic returns returns of AAPL stock
 log_return = np.log(1 + df.pct_change())
 log_return
 
+# Computing the drift
 u = log_return.mean()
 v = log_return.var()
 
 drift = u - (0.5*v)
 drift
 
+# Computing the variance and Daily Returns
 stdev = log_return.std()
-days = 50
+days = 50  #We are predicting AAPL's stock prices 50 days into the future
 num_simulations = 1000
 Z = norm.ppf(np.random.rand(days,num_simulations))
 Z
@@ -33,6 +39,7 @@ daily_returns = np.exp(drift + stdev*Z)
 
 plt.plot(daily_returns)
 
+# Calulating the stock price for every trial
 price_paths = np.zeros_like(daily_returns)
 price_paths[0] = df.iloc[-1]
 for t in range(1, days):
@@ -41,4 +48,6 @@ for t in range(1, days):
 price_paths
 
 plt.plot(price_paths)
+
+
 
