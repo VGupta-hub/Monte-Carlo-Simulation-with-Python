4b244dfe22c604251325674227328d58b4b831fc
Vaishali Gupta <83883988+VGupta-hub@users.noreply.github.com>
2021-08-30 14:10:41 -0400

    Create Code.py

---
+++ Code.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+from pandas_datareader import data as web
+import matplotlib.pyplot as plt
+import seaborn as sns
+import scipy
+from scipy.stats import norm
+%matplotlib notebook
+
+df = pd.read_csv("C:/Users/vgupt/Desktop/Website Project Folder/MCS/AAPL.csv")['Adj Close']
+df
+
+plt.xlabel("Date")
+plt.ylabel("Price")
+df.plot(figsize=(15,6))
+
+log_return = np.log(1 + df.pct_change())
+log_return
+
+u = log_return.mean()
+v = log_return.var()
+
+drift = u - (0.5*v)
+drift
+
+stdev = log_return.std()
+days = 50
+num_simulations = 1000
+Z = norm.ppf(np.random.rand(days,num_simulations))
+Z
+
+daily_returns = np.exp(drift + stdev*Z)
+
+plt.plot(daily_returns)
+
+price_paths = np.zeros_like(daily_returns)
+price_paths[0] = df.iloc[-1]
+for t in range(1, days):
+    price_paths[t] = price_paths[t-1]*daily_returns[t]
+    
+price_paths
+
+plt.plot(price_paths)
+
